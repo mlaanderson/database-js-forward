0500fc5d66118f4129788841f7ec78e3aee9315a
Michael Anderson <Michael.Anderson@amesburytruth.com>
2017-12-13 13:19:12 -0600

    Added support for transactions

--- index.js
+++ index.js
@@ -26,6 +26,72 @@ class ForwardSQL {
         return this.__connection.close();
     }
 
+    /**
+     * Indicates whether the underlying driver can support transactions;
+     * 
+     * @returns {boolean}
+     * @memberof Connection
+     */
+    isTransactionSupported() {
+        debug('ISTRANSACTIONSUPPORTED: %s', this.__connection.isTransactionSupported());
+        return this.__connection.isTransactionSupported();
+    }
+
+    /**
+     * Returns true if the underlying driver is in a transaction, false
+     * if it does not support transactions or is not in a transaction.
+     * 
+     * @returns {boolean}
+     * @memberof Connection
+     */
+    inTransaction() {
+        debug('INTRANSACTION: %s', this.__connection.inTransaction());
+        return this.__connection.inTransaction();
+    }
+
+    /**
+     * Returns a boolean promise: true if a transaction was started and
+     * false if it was not started. Transactions can fail to start if
+     * another transaction is already running or if the driver does 
+     * not support transactions.
+     * 
+     * @returns {Promise<boolean>}
+     * @memberof Connection
+     */
+    beginTransaction() {
+        debug('BEGINTRANSACTION');
+        return this.__connection.beginTransaction();
+    }
+
+    /**
+     * Returns a boolean promise: true if a transaction was committed and
+     * false if one was not committed. Transactions can fail to commit if
+     * no transaction was started, or if the driver does not support
+     * transactions.
+     * 
+     * @returns {Promise<boolean>}
+     * @memberof Connection
+     */
+    commit() {
+        debug('COMMIT');
+        return this.__connection.commit();
+    }
+
+
+    /**
+     * Returns a boolean promise: true if a transaction was rolled back and
+     * false if one was not rolled back. Transactions can fail to roll back if
+     * no transaction was started, or if the driver does not support
+     * transactions.
+     * 
+     * @returns {Promise<boolean>}
+     * @memberof Connection
+     */
+    rollback() {
+        debug('ROLLBACK');
+        return this.__connection.rollback();
+    }
+
     static chain(driver) {
         return {
             open: function(connection) {
